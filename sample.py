e6049fd1b0f37d39b3540435dd2e6409607eb5e3
Ruchi <batra.ruchi@gmail.com>
2023-09-19 09:15:16 -0400

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,19 @@
+import openai
+import streamlit as st
+openai.api_key="your API Key"
+
+def generate_response(myprompt):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=myprompt,
+        temperature=0.3,
+        max_tokens=1024
+    )
+    return response.choices[0].text.strip()
+
+def main ():
+    myprompt = input("How can I help you? \n")
+    print(generate_response(myprompt))
+
+if __name__ == "__main__":
+    main()
