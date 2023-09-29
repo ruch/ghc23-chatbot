19cd7bfdc1b69c4ca5380d57a9b9c30eeac7a6e4
ruch <www_ruchi@yahoo.co.in>
2023-09-29 11:02:39 -0400

    demo original version

--- demo.py
+++ demo.py
@@ -1,21 +1,19 @@
 import openai
-import streamlit as st
+openai.api_key="Your API Key here"
 
 def generate_response(myprompt):
     response = openai.Completion.create(
         engine="text-davinci-003",
-        prompt="You are a trained psychiatrist. Answer the prompts accordingly. ###" + myprompt,
-        temperature=.3,
-        max_tokens=1023
+        prompt=myprompt,
+        temperature=.1,
+        max_tokens=1024
     )
+    print (response.choices)
     return response.choices[0].text.strip()
 
 def main ():
-    st.title("Friendly Chat Therapist")
-    myprompt = st.text_input("I'm Dr Fraiser Crane. I'm listening. How can I help you? \n")
-    # print(generate_response(myprompt))
-    if st.button("Submit"):
-        st.write(generate_response(myprompt))
+    myprompt = input("How can I help you? \n")
+    print(generate_response(myprompt))
 
 if __name__ == "__main__":
     main()
